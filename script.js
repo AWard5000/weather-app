8f0a05c418cd1cdcc7f6b9b203d328d4b219be47
AWard5000 <122940886+AWard5000@users.noreply.github.com>
2023-01-22 11:54:04 -0500

    Update script.js
    changes made to correct forecast code

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let apiKey = "6d48t90aao34607b488607a8df81d2bd"
-let apiUrl = "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"
-
+let apiKey = "6d48t90aao34607b488607a8df81d2bd";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
 
 function formatDate(date) {
   let hours = date.getHours();
@@ -20,41 +20,67 @@ function formatDate(date) {
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday"
+    "Saturday",
   ];
   let day = days[dayIndex];
 
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(time) {
+  let date = new Date(time);
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return day;
+}
+
 function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecast = response.data.daily;
-  
+  let forecastHTML = "";
+
   let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
-  forecast.foreEach(function (forecastDay, Index) {
+  forecast.forEach(function (forecastDay, index) {
     if (index < 5) {
       forecastHTML =
-      forecastHTML +
-   
-  `
+        forecastHTML +
+        `
   <div class="col-2">
-    <div class="weather-forecast-date">${formatday(forecastDay.dt)}</div>
-    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.[0]icon}.png" alt="" />
+    <div class="weather-forecast-date">${formatDay(forecastDay.time)}</div>
+    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+      forecastDay.condition.icon
+    }.png" alt="" />
       <div class="weather-forecast-temperatures">
-     <span class="weather-forecast-temp-max"> ${Math.round(forecastDay.temp.max)}°</span>
-     <span class="weather-forecast-temp-min"> ${Math.round(forecastDay.temp.min)}°</span>
+     <span class="weather-forecast-temp-max"> ${Math.round(
+       forecastDay.temperature.maximum
+     )}°</span>
+     <span class="weather-forecast-temp-min"> ${Math.round(
+       forecastDay.temperature.minimum
+     )}°</span>
      </div>
   </div>
 `;
-});
-forecastHTML = forecastHTML + `</div>`;
-forecastElement.innerHTML = forecastHTML;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
-  function getForecast(coordinates) {
-  let apiKey = "6d48t90aao34607b488607a8df81d2bd"
-  let apiUrl ="https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}& units=metric";
+function getForecast(coordinates) {
+  let lon = coordinates.longitude;
+  let lat = coordinates.latitude;
+  let apiKey = "6d48t90aao34607b488607a8df81d2bd";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -76,11 +102,11 @@ function displayWeatherCondition(response) {
   document.querySelector("#description").innerHTML =
     response.data.condition.description;
 
-    getForecast(response.data.coord);
+  getForecast(response.data.coordinates);
 }
 
 function searchCity(city) {
-  let apiKey = "6d48t90aao34607b488607a8df81d2bd"
+  let apiKey = "6d48t90aao34607b488607a8df81d2bd";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayWeatherCondition);
@@ -121,12 +147,11 @@ let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
 function searchLocation(position) {
-  let apiKey = "6d48t90aao34607b488607a8df81d2bd"
+  let apiKey = "6d48t90aao34607b488607a8df81d2bd";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?lat=${position.coords.latitude}&lon=${position.coords.longitude}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayWeatherCondition);
 }
-
 
 function getCurrentLocation(event) {
   event.preventDefault();
@@ -136,7 +161,7 @@ function getCurrentLocation(event) {
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
-searchCity("Los Angeles");
+searchCity("Los Angeles")
 
 
 
